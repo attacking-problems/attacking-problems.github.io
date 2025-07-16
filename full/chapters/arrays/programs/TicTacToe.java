fa72cd5db4fe8933f32eea74ea35695d5ce73fa5
Barry <bwittman@gmail.com>
2025-07-15 21:48:24 -0400

    Updated arrays chapter to use braces, even for single-line if statements and loops.

--- full/chapters/arrays/programs/TicTacToe.java
+++ full/chapters/arrays/programs/TicTacToe.java
@@ -2,29 +2,31 @@ import java.util.*;
 
 public class TicTacToe {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);  //<.>
-        char[][] board = new char[3][3];      //<.>
-        for(int i = 0; i < board.length; i++) //<.>
-            for(int j = 0; j < board[0].length; j++)
+        Scanner in = new Scanner(System.in);  // <.>
+        char[][] board = new char[3][3]; // <.>
+        for (int i = 0; i < board.length; ++i) { // <.>
+            for (int j = 0; j < board[0].length; ++j) {
                 board[i][j] = ' ';
-        boolean turn = true;        //<.>
+            }
+        }
+        boolean turn = true; // <.>
         boolean gameOver = false;
-        int row, column, moves = 0; //<.>
+        int row, column, moves = 0; // <.>
         char shape;
 
-        while(!gameOver) {
+        while (!gameOver) {
             shape = turn ? 'X' : 'O';
-            System.out.print(shape + "'s turn.  Enter row (0-2): "); //<.>
+            System.out.print(shape + "'s turn.  Enter row (0-2): "); // <.>
             row = in.nextInt();
             System.out.print("Enter column (0-2): ");
             column = in.nextInt();
-            if(board[row][column] != ' ')   //<.>
+            if (board[row][column] != ' ') {  // <.>
                 System.out.println("Illegal move");
-            else {  
-                board[row][column] = shape; //<.>
-                moves++;            
+            } else {  
+                board[row][column] = shape; // <.>
+                ++moves;            
                 turn = !turn;
-                // Print board		<.>
+                // Print board <.>
                 System.out.println(board[0][0] + "|" 
                          + board[0][1] + "|" + board[0][2]);
                 System.out.println("-----");
@@ -33,26 +35,32 @@ public class TicTacToe {
                 System.out.println("-----");
                 System.out.println(board[2][0] + "|" 
                          + board[2][1] + "|" + board[2][2] + "\n");             
-                // Check rows			<.>
-                for(int i = 0; i < board.length; i++)
+                // Check rows <.>
+                for (int i = 0; i < board.length; ++i) {
                     if(board[i][0] == shape && board[i][1] == shape
-                        && board[i][2] == shape)
+                        && board[i][2] == shape) {
                         gameOver = true;
-                // Check column			<.>
-                for(int i = 0; i < board[0].length; i++)
+                    }
+                }
+                // Check column	<.>
+                for (int i = 0; i < board[0].length; ++i) {
                     if(board[0][i] == shape && board[1][i] == shape
-                        && board[2][i] == shape)
+                        && board[2][i] == shape) {
                         gameOver = true;
-                // Check diagonals		<.>
+                    }
+                }
+                // Check diagonals <.>
                 if(board[0][0] == shape && board[1][1] == shape
-                    && board[2][2] == shape)
+                    && board[2][2] == shape) {
                     gameOver = true;
+                }
                 if(board[0][2] == shape && board[1][1] == shape
-                    && board[2][0] == shape)
-                    gameOver = true;            
-                if(gameOver)      		//<.>
+                    && board[2][0] == shape) {
+                    gameOver = true;          
+                }  
+                if(gameOver) { // <.>
                     System.out.println(shape + " wins!");
-                else if(moves == 9){	//<.>
+                } else if(moves == 9) {	// <.>
                     gameOver = true;            
                     System.out.println("Tie game!");        
                 }               
